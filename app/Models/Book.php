53756b835b9eff4f9ea80943ae069cd12e08d780
Serhii Bezpalko <serhii.bezpalko@protonmail.com>
2025-07-18 21:08:49 +0300

    add many to many ralationship

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Book extends Model
 {
-    //
+    public function authors(): BelongsToMany
+    {
+        return $this->belongsToMany(Author::class);
+    }
 }
---
+++ database/migrations/2025_07_18_164155_author_book.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("author_book", function (Blueprint $table) {
+            $table->bigInteger("author_id");
+            $table->bigInteger("book_id");
+            $table->unique(["author_id", "book_id"]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists("author_book");
+    }
+};
